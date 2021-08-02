6d44071d432a2fdb11b29c8db1ea4175fa4cdebd
Practicante Desarrollo <prdesarrollo@comodisimos.com.co>
2021-08-02 17:21:03 -0500

    Lista de datos en index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,88 @@
 <template>
-  <div id="app">
-    <q-page class="row items-center justify-evenly">
-      <q-btn color="primary" icon="check" label="OK" @click="listOfUnassigment" />
-    </q-page>
+  <q-page>
+    <div id='app'>
+      <!-- <table>
+        <tr>
+          <td v-for='log in logs[0]' :key='log.id'>
+            {{ log.user }}
+          </td>
+        </tr>
+      </table> -->
+      <div class="q-pa-md">
+        <q-table
+          title="Acciones"
+          :rows="logs[0]"
+          :columns="columns"
+          row-key="name"
+          dark
+          color="amber"
+        />
   </div>
+    </div>
+  </q-page>
 </template>
 
-<script lang="ts">
+<script>
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 export default {
+  name: 'tutorial',
   el: 'app',
-  mounted: {
+  data () {
+    return {
+      logs: [],
+      user: '',
+      columns: [
+        {
+          name: 'id',
+          label: 'Id Log',
+          align: 'center',
+          field: 'id',
+          sortable: true
+        },
+        { name: 'type', align: 'center', label: 'Tipo', field: 'type', sortable: true },
+        { name: 'user', align: 'center', label: 'Usuario responsable', field: 'user', sortable: true },
+        { name: 'employee', align: 'center', label: 'Empleado asignado', field: 'employee', sortable: true},
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        { name: 'characteristics', align: 'center', label: 'Characteristics', field: row => this.comoQuiera(row), sortable: true },
+        { name: 'reason', align: 'center', label: 'Razon', field: 'reason', sortable: true },
+        { name: 'inventory_id', align: 'center', label: 'Equipo', field: 'inventory_id', sortable: true }
+      ]
+    }
+  },
+  created () {
+    // this.listOfUnassigment()
     this.listOfUnassigment()
   },
   methods: {
-    async listOfUnassigment (): Promise<void> {
-      try {
-        const data = await fetch(
-          'http://localhost/sistemaDeInventario/public/api/showLogs'
-        )
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const responseData = await data.json()
-        console.log(responseData)
-      } catch (error) {
-        console.log(error)
-      }
+    // async listOfUnassigment(): Promise<void> {
+    //   console.log('hola que haces');
+    //   try {
+    //     const data = await fetch(
+    //       'http://localhost/sistemaDeInventario/public/api/showLogs'
+    //     );
+    //     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    //     const responseData = await data.json();
+    //     console.log(responseData);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // },
+    listOfUnassigment () {
+      this.$axios
+        .get('http://localhost/sistemaDeInventario/public/api/showLogs')
+        .then((res) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          this.logs = res.data
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    },
+    comoQuiera (row) {
+      console.log('aaa', row)
+      
+      return 'c'
     }
   }
 }
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,6 +6,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/Index.vue') }]
   },
+  {
+    path: '/prueba',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/prueba.vue') }]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
